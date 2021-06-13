013c4a338777ff8281f95da84583919d09bf9a0f
Hernan Berisso <hberisso1@gmail.com>
2021-06-12 23:44:09 -0400

    connected to pokemon api, displaying all pokemon on home page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,43 @@
-import Head from "next/head"
 import Layout from "../components/Layout"
+import Link from "next/link"
 
-export default function Home() {
+export default function Home({ pokemon }) {
 	return (
 		<Layout title="NextJS Pokedex">
-			<h1>NextJS Pokedex</h1>
+			<h1 className="text-4xl mb-8 text-center">NextJS Pokedex</h1>
+			<ul>
+				{pokemon.map((pokeman, index) => (
+					<li key={index}>
+						<Link href={`/pokemon?id=${index + 1}`}>
+							<a>
+								<img src={pokeman.image} alt={pokeman.name} />
+								<span>{index + 1}</span>
+								{pokeman.name}
+							</a>
+						</Link>
+					</li>
+				))}
+			</ul>
 		</Layout>
 	)
+}
+
+export async function getStaticProps(context) {
+	try {
+		const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=150")
+		const { results } = await res.json()
+		const pokemon = results.map((result, index) => {
+			const paddedIndex = ("00" + (index + 1)).slice(-3)
+			const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${paddedIndex}.png`
+			return {
+				...result,
+				image,
+			}
+		})
+		return {
+			props: { pokemon },
+		}
+	} catch (error) {
+		console.error(error)
+	}
 }
